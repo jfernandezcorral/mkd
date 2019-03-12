3941199ba43c234d2cfd632048a99afe36b5f42f
jfernandezcorral <jfernandezcorral@gmail.com>
2019-03-12 20:18:17 +0100

    mas

--- app/main.js
+++ app/main.js
@@ -1,8 +1,19 @@
-const {app, BrowserWindow} = require ('electron')
+const {app, BrowserWindow, dialog} = require ('electron')
+const getFileFromUser = () =>{
+    dialog.showOpenDialog(
+        mainW,{
+        properties: ['openFile']},
+        console.log
+    )
+}
 let mainW = null
 app.on('ready', ()=>{
-    mainW = new BrowserWindow()
+    mainW = new BrowserWindow({show: false})
     mainW.loadFile('./app/index.html')
+    mainW.once('ready-to-show', () => {
+        mainW.show();
+        getFileFromUser();
+    });
     mainW.on('closed',()=>{
         mainW = null
     })
