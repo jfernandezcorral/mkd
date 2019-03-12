67b4887869a946e2e4fd114c2646cd69a36a2895
jfernandezcorral <jfernandezcorral@gmail.com>
2019-03-12 19:40:08 +0100

    debug

--- app/renderer.js
+++ app/renderer.js
@@ -1,1 +1,11 @@
-console.log('hola')
+const marked = require('marked');
+const markdownView = document.querySelector('#markdown')
+const htmlView = document.querySelector('#html')
+const newFileButton = document.querySelector('#new-file')
+const renderMarkdownToHtml = (markdown) => {
+    htmlView.innerHTML = marked(markdown, { sanitize: true })
+}
+markdownView.addEventListener('keyup', (event) => {
+    const currentContent = event.target.value
+    renderMarkdownToHtml(currentContent)
+})
